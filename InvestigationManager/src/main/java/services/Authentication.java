be4617808ced952b8f80f68ad7f705d9d48c2ddf
Hyvalker Allan Correia do Amaral <hyva.allan@gmail.com>
2025-11-16 14:05:34 -0300

    refactor: split addDetective logic into helper methods

--- InvestigationManager/src/main/java/services/Authentication.java
+++ InvestigationManager/src/main/java/services/Authentication.java
@@ -2,6 +2,7 @@ package services;
 
 import model.Detective;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Authentication {
@@ -15,22 +16,25 @@ public class Authentication {
     public boolean loginAuthentication(Scanner scanner) {
 
         while (true) {
+            try {
+                System.out.println("Digite o nome de usuário: ");
+                String username = scanner.nextLine();
+                System.out.println("Digite a senha: ");
+                String password = scanner.nextLine();
+                scanner.nextLine();
 
-            System.out.println("Digite o nome de usuário: ");
-            String username = scanner.nextLine();
-            System.out.println("Digite a senha: ");
-            int password = scanner.nextInt();
-            scanner.nextLine();
-
-            if (!detective.getSystemUsername().equalsIgnoreCase(username)) {
-                System.out.println("Nome de usuário inválido. Tente novamente: ");
-                continue;
-            } else if (detective.getSystemPassword() != password) {
-                System.out.println("Senha inválida. Tente novamente: ");
-                continue;
-            } else {
-                System.out.println("Login efetuado com sucesso.");
-                return true;
+                if (!detective.getSystemUsername().equalsIgnoreCase(username)) {
+                    System.out.println("Nome de usuário inválido. Tente novamente: ");
+                    continue;
+                } else if (detective.getSystemPassword().equalsIgnoreCase(password)) {
+                    System.out.println("Senha inválida. Tente novamente: ");
+                    continue;
+                } else {
+                    System.out.println("Login efetuado com sucesso.");
+                    return true;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Tente novamente.");
             }
         }
     }
